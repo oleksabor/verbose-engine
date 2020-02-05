b5edcc4c274ef5cfa9845467284f0c723b363731
oleksii borodai <oleksa.borodie@gmail.com>
2020-02-05 16:55:34 +0200

    .net standard project and .net framework test project

---
+++ TestLibrary/Class1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    [TestClass]
+    public class Class1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
